f36aa443b608fc560a9e870c71b73829612a4fa8
Flavio A <FlavioAAndres24@gmail.com>
2020-10-08 20:10:23 -0500

    📦 NEW: adding interactive console to aws-accounts

--- src/commands/aws-accounts.js
+++ src/commands/aws-accounts.js
@@ -1,5 +1,7 @@
 const { Command, flags } = require("@oclif/command");
 const AWSCredentials = require("manage-aws-credentials");
+const { Input } = require('enquirer')
+const Table = require('cli-table3');
 
 class AWSAccounts extends Command {
   static args = [
@@ -12,84 +14,122 @@ class AWSAccounts extends Command {
     },
   ];
 
+  InputName = (action)=>new Input({
+    message: "What name you wants to " + action,
+    initial: 'random-user'
+  });
+  InputSecretKey = ()=> new Input({
+    message: "=> Secret Key",
+    initial: 'uuiid/uuid@uuid:random',
+    required: true,
+  });
+  InputAccsessKey = ()=> new Input({
+    message: "=> Access Key",
+    initial: 'AKIA123456',
+    required: true,
+  });
+
+  handleAddAction = async ({name, access_key, secret_access_key})=>{
+    if(!name){
+      name = await this.InputName('add').run()
+    }
+    if(!access_key){
+      access_key = await this.InputAccsessKey().run()
+    }
+    if(!secret_access_key){
+      secret_access_key = await this.InputSecretKey().run()
+    }
+
+    AWSCredentials.add_profile(name, {
+      access_key,
+      secret_access_key,
+    });
+    const fileSaved = AWSCredentials.save_file();
+    this.log(
+      fileSaved === 1
+        ? `Your Account ${name} was successfully saved`
+        : "There were issues saving your new account"
+    );
+  }
+
+  handleRemoveAction = async({name}) =>{
+    if (!name) {
+      name = await this.InputName('remove').run()
+    }
+    AWSCredentials.delete_profile(name);
+    const fileSaved = AWSCredentials.save_file();
+    this.log(
+      fileSaved === 1
+        ? `Your Account ${name} was successfully deleted`
+        : "There were issues removing your new account"
+    );
+  }
+
+  handleShowAction = async () =>{
+    const users_aws = AWSCredentials.serialize_credentials("object") || []; 
+    
+    const AccountsTable = new Table({
+      head: ["Account Name", "Access Key"]
+    });
+    const parsedData = users_aws.map(user=>[user.name.replace(/(\[)|(\])/g, ''), user.access_key])
+    AccountsTable.push(...parsedData);
+
+    this.log(AccountsTable.toString())
+  }  
+
+  handleToAction = async ({name})=>{
+    if (!name) {
+      name = await this.InputName('move').run()
+    }
+    const credentials = AWSCredentials.serialize_credentials("object");
+    
+    //Look for the new default profile
+    const credentialFilter = credentials.filter(
+      (_credential) =>{
+        return _credential.name.replace(/(\[)|(\])/g, '') === name
+      }
+    );
+
+    if (credentialFilter.length === 1) {
+      const credential = credentialFilter[0];
+      delete credential.name;
+
+      //Editing the current default profile
+      AWSCredentials.edit_profile("default", credential);
+
+      //Save the new profile object
+      const fileSaved = AWSCredentials.save_file();
+
+      this.log(
+        fileSaved === 1
+          ? "Your default account was successfully switched"
+          : "There were an issue switching your default account"
+      );
+    }
+    
+    throw new Error(
+      `Looks like that there is not a profile with the given name, use the command aws-accounts show to get the list of current profiles`
+    );
+  }
+
   async run() {
     const { flags, args } = this.parse(AWSAccounts);
-    const name = flags.name;
-    const access_key = flags.key;
-    const secret_access_key = flags.secret;
+    const {
+      name, 
+    } = flags
 
     switch (args.action) {
       case "add": {
-        if (name && access_key && secret_access_key) {
-          AWSCredentials.add_profile(name, {
-            access_key,
-            secret_access_key,
-          });
-          const fileSaved = AWSCredentials.save_file();
-          this.log(
-            fileSaved === 1
-              ? "Account successfully saved"
-              : "There were an issue saving your new account"
-          );
-          break;
-        }
-        throw new Error(
-          `The flags name, key, and secret are required, use the flag --help for documentation`
-        );
+        return await this.handleAddAction(flags)        
       }
       case "remove": {
-        if (name) {
-          AWSCredentials.delete_profile(name);
-          const fileSaved = AWSCredentials.save_file();
-          this.log(
-            fileSaved === 1
-              ? "Account successfully removed"
-              : "There were an issue removing your new account"
-          );
-          break;
-        }
-        throw new Error(
-          `The flags name is required, use the flag --help for documentation`
-        );
+        return await this.handleRemoveAction(flags)
       }
       case "show": {
-        this.log(AWSCredentials.serialize_credentials("object"));
-        break;
+        return this.handleShowAction()
       }
       case "to": {
-        if (name) {
-          //Get all profiles as a object
-          const credentials = AWSCredentials.serialize_credentials("object");
-          //Look for the new default profile
-          const credentialFilter = credentials.filter(
-            (_credential) =>
-              _credential.name.replace("[", "").replace("]", "") === name
-          );
-
-          if (credentialFilter.length === 1) {
-            const credential = credentialFilter[0];
-            delete credential.name;
-
-            //Editing the current default profile
-            AWSCredentials.edit_profile("default", credential);
-
-            //Save the new profile object
-            const fileSaved = AWSCredentials.save_file();
-
-            this.log(
-              fileSaved === 1
-                ? "Your default account was successfully switched"
-                : "There were an issue switching your default account"
-            );
-            break;
-          }
-          throw new Error(
-            `Looks like that there is not a profile with the given name, use the command aws-accounts show to get the list of current profiles`
-          );
-        }
-        throw new Error(
-          `The flags name is required, use the flag --help for documentation`
-        );
+        return this.handleToAction(flags)
       }
       default: {
         break;
