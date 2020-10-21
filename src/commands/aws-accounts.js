e3c79e60f45ac4fbad3f3a9c0807e9a153f116ec
Andres <andresmorelos123@gmail.com>
2020-10-21 14:21:45 -0500

    Show select at aws-accounts Remove and To actions.

--- src/commands/aws-accounts.js
+++ src/commands/aws-accounts.js
@@ -1,6 +1,6 @@
 const { Command, flags } = require("@oclif/command");
 const AWSCredentials = require("manage-aws-credentials");
-const { Input } = require('enquirer')
+const { Input, Select } = require('enquirer')
 const Table = require('cli-table3');
 
 class AWSAccounts extends Command {
@@ -29,6 +29,13 @@ class AWSAccounts extends Command {
     required: true,
   });
 
+  SelectName = (action, names) => new Select({
+    name: "Repository",
+    message: `What name you wants to ${action}?`,
+    type: "list",
+    choices: [...names],
+  })
+
   handleAddAction = async ({name, key, secret})=>{
     if(!name){
       name = await this.InputName('add').run()
@@ -54,7 +61,9 @@ class AWSAccounts extends Command {
 
   handleRemoveAction = async({name}) =>{
     if (!name) {
-      name = await this.InputName('remove').run()
+      const users_aws = AWSCredentials.serialize_credentials("object") || []; 
+      const names = users_aws.filter(user => user.name.replace(/(\[)|(\])/g, '') !== 'default' ).map(user=>user.name.replace(/(\[)|(\])/g, ''))
+      name = await this.SelectName('move',names).run()
     }
     AWSCredentials.delete_profile(name);
     const fileSaved = AWSCredentials.save_file();
@@ -79,7 +88,9 @@ class AWSAccounts extends Command {
 
   handleToAction = async ({name})=>{
     if (!name) {
-      name = await this.InputName('move').run()
+      const users_aws = AWSCredentials.serialize_credentials("object") || []; 
+      const names = users_aws.filter(user => user.name.replace(/(\[)|(\])/g, '') !== 'default' ).map(user=>user.name.replace(/(\[)|(\])/g, ''))
+      name = await this.SelectName('move',names).run()
     }
     const credentials = AWSCredentials.serialize_credentials("object");
     
